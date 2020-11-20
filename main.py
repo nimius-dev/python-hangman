e139da27a89204bd786fe4dd4454f861526352af
lucas <l.wieling@gmx.de>
2020-11-20 18:10:46 +0100

    finished functionality (finishGame method, guess method) removed hard coded strings and fixed formatting added TODO removed obsolete handleOutput() method

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import choice
 from string import ascii_lowercase
 
 with open("words", "r") as words:
-    word_list = [_ for _ in words]
+    word_list = [_.rstrip() for _ in words]
 end = False
 max_guesses = 10
 symbol = "-"
@@ -13,43 +13,48 @@ greeting = "Welcome to \n\n" \
            "Enter \"q\" to quit\n" \
            "or press enter to \n" \
            "start a new game"
-input_error_message = 'Please enter'
-starting_message= f'The secret word has {num} letters. Good Luck!'
-
+input_error_message = "Please enter a single letter"
+starting_message = 'The secret word has {num} letters. Good Luck!'
+win_message = 'Congratulations, you won! The secret word was {secret}. You had {num} wrong guesses.'
+loss_message = 'Sorry, no more tries left. You lost! The secret word was {secret}.'
+guess_message = "Guess a letter! You have {num} wrong guesses so far.\n"
+right_message = "Good job, \"{letter}\" is in the secret word"
 
 class Game:
     def __init__(self):
         self.tries = max_guesses
         self.secret_word = choice(word_list)
         self.word_label = symbol * len(self.secret_word)
-        print(f'The secret word has {len(self.secret_word)} letters. Good Luck!')
+        print(starting_message.format(num=len(self.secret_word)))
         self.logic()
 
     def logic(self):
         while self.word_label != self.secret_word and self.tries > 0:
             self.showOutput()
             self.guess()
-            self.handleOutput()
         self.finishGame()
 
     def showOutput(self):
-        #TODO: add and display list for already entered letters.
-        print(self.word_label)
+        # TODO: add and display list for already entered letters.
+        print(self.word_label.upper())
 
     def guess(self):
         print()
-        guess = input("Guess a letter! ").lower()
+        guess = input(guess_message.format(num=max_guesses-self.tries)).lower()  # TODO: Let user guess whole word
         if guess in ascii_lowercase and len(guess) == 1:
             if guess in self.secret_word:
-                self.word_label = [s if s == guess else symbol for s in self.secret_word]
+                print(right_message.format(letter=guess))
+                self.word_label = ''.join([s if s == guess else self.word_label[self.secret_word.index(s)] for s in self.secret_word])
+            else:
+                self.tries -= 1
         else:
-            print("Please ")
-
-    def handleOutput(self):
-        pass
+            print(input_error_message)
 
     def finishGame(self):
-        pass
+        if self.tries > 0:
+            print(win_message.format(secret=self.secret_word, num=max_guesses-self.tries))
+        else:
+            print(loss_message.format(secret=self.secret_word))
 
 
 def main():
