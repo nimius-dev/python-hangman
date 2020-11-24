2c40a48ed08bbfc39d8c61844df6dd5b2c2918ec
lucas <l.wieling@gmx.de>
2020-11-24 13:54:21 +0100

    did TODOs: -Added list for already guessed letters -addedd support for solving with single word

--- main.py
+++ main.py
@@ -19,12 +19,15 @@ win_message = 'Congratulations, you won! The secret word was {secret}. You had {
 loss_message = 'Sorry, no more tries left. You lost! The secret word was {secret}.'
 guess_message = "Guess a letter! You have {num} wrong guesses so far.\n"
 right_message = "Good job, \"{letter}\" is in the secret word"
+wrong_message = "Sadly, \"{letter}\" isn\'t in the secret word"
+already_guessed_message = "You already have guessed \"{letter}\", try another letter!"
 
 class Game:
     def __init__(self):
         self.tries = max_guesses
         self.secret_word = choice(word_list)
         self.word_label = symbol * len(self.secret_word)
+        self.guess_list = set()
         print(starting_message.format(num=len(self.secret_word)))
         self.logic()
 
@@ -35,18 +38,26 @@ class Game:
         self.finishGame()
 
     def showOutput(self):
-        # TODO: add and display list for already entered letters.
         print(self.word_label.upper())
+        print("Remaining letters to guess:")
+        print(''.join([letter if letter not in self.guess_list else '_' for letter in ascii_lowercase]))
 
     def guess(self):
         print()
-        guess = input(guess_message.format(num=max_guesses-self.tries)).lower()  # TODO: Let user guess whole word
-        if guess in ascii_lowercase and len(guess) == 1:
-            if guess in self.secret_word:
-                print(right_message.format(letter=guess))
-                self.word_label = ''.join([s if s == guess else self.word_label[self.secret_word.index(s)] for s in self.secret_word])
+        guess = input(guess_message.format(num=max_guesses-self.tries)).lower()
+        if guess == self.secret_word:
+            self.word_label = self.secret_word
+        elif guess in ascii_lowercase and len(guess) == 1:
+            if guess in self.guess_list:
+                print(already_guessed_message.format(letter=guess))
             else:
-                self.tries -= 1
+                self.guess_list.add(guess)
+                if guess in self.secret_word:
+                    print(right_message.format(letter=guess))
+                    self.word_label = ''.join([s if s == guess else self.word_label[self.secret_word.index(s)] for s in self.secret_word])
+                else:
+                    print(wrong_message.format(letter=guess))
+                    self.tries -= 1
         else:
             print(input_error_message)
 
